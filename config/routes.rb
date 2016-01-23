b9814630ad05837f1c51607742b658c7a72266f2
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-23 12:51:56 +0000

    Update routes and add config for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # Root route
   root to: 'meetings#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Authentication
+  devise_for :users, skip: [:sessions, :passwords, :confirmations, :registrations],  controllers: {omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'}
+  as :user do
+    # User settings
+    get '/:id/settings' => 'users/registrations#edit',   as: 'edit_user_registration'
+    patch '/:id/settings' => 'users/registrations#update', as: 'update_user_registeration'
+
+    # Omniauth
+    match 'auth/:provider/callback', to: 'users/sessions#create', via: [:get, :post]
+    match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  end
+
+  # Meetings resource
+  resources :meetings, only: [:index, :show]
 
   # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 end
